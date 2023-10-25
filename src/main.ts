2d98ccfdf72bc40069a779a22b1e02cec672be39
abolfazl <abolfazlakbarzadeh80@gmail.com>
2023-10-25 14:28:12 +0330

    - remove tailwindcss - add and setup bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import "assets/global.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
+import routes from "./routes";
+import BootstrapVue from "bootstrap-vue-3";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 const pinia = createPinia();
-
-createApp(App).use(pinia).mount("#app");
+createApp(App).use(BootstrapVue).use(routes).use(pinia).mount("#app");
