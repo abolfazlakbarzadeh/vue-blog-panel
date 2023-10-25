9b5eb496d9a4ec84b6b618bf0e907fca14ecddce
abolfazl <abolfazlakbarzadeh80@gmail.com>
2023-10-25 12:11:46 +0330

    - fix eslint problem - add tsconfigPaths vite plugin - add global style

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue(), eslintPlugin(), tsconfigPaths()],
 });
