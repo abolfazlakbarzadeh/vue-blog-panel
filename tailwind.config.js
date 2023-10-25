027d3215cbd3c4f68e7be37be29d7005fc76627a
abolfazl <abolfazlakbarzadeh80@gmail.com>
2023-10-25 12:21:28 +0330

    - add tailwind packages and setup them - fix eslint issue

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.tsx", "src/**/*.vue"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
