78fb42fec19e09f7fc473cd9608c42bd383e959e
abolfazl <abolfazlakbarzadeh80@gmail.com>
2023-10-25 22:39:39 +0330

    fix: minor UI issues

--- src/components/dashboard/the-header.vue
+++ src/components/dashboard/the-header.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="the-header d-flex align-items-center justify-content-between">
+  <div class="the-header navbar navbar-expand-lg bg-dark">
     <div class="d-flex align-items-center gap-3">
-      <h2 class="hr-title">Arvan Challenge</h2>
-      <div class="hr-username">Welcome ...</div>
+      <h2 class="navbar-brand text-white hr-title">Arvan Challenge</h2>
+      <div class="hr-username text-white fw-light">Welcome ...</div>
     </div>
-    <div class="hr-actions">
+    <div class="hr-actions ml-auto">
       <div class="btn btn-outline-info">Logout</div>
     </div>
   </div>
